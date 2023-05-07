0262d6e19ef85b3c922966d389a4a1814bbebfe1
tcousik <contejas1@gmail.com>
2023-05-06 23:07:50 -0600

    fixed path

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -15,7 +15,7 @@ const ProfilePage = () => {
     const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
 
     const getUser = async () => {
-        const response = await fetch(`http://localhost:3001/users/${userId}`, {
+        const response = await fetch(`https://final-back-3td9.onrender.com/users/${userId}`, {
             method: "GET",
             headers: { Authorization: `Bearer ${token}` },
         });
@@ -25,7 +25,7 @@ const ProfilePage = () => {
 
     useEffect(() => {
         getUser();
-    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+    }, []);
 
     if (!user) return null;
 
